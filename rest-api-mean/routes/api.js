b82f3a7e7041848b9da0aaf7526dfa583afb0933
Tushar <gupta.tus@husky.neu.edu>
2018-09-26 13:43:35 -0400

    updation of entries done

--- rest-api-mean/routes/api.js
+++ rest-api-mean/routes/api.js
@@ -4,32 +4,37 @@ const Ninja = require('../models/ninja')
 
 
 //get new ninja from db
-router.get('/ninjas',(req,res,next)=>{
-        res.send({type:'geting the ninjas'});
+router.get('/ninjas', (req, res, next) => {
+    res.send({ type: 'geting the ninjas' });
 })
 
 
 //add a new ninja to db
-router.post('/ninjas',(req,res,next)=>{
+router.post('/ninjas', (req, res, next) => {
     console.log(req.body);
-    Ninja.create(req.body).then(function(ninja){
+    Ninja.create(req.body).then(function (ninja) {
         res.send(ninja);
     }).catch(next)
 })
 
 
 //update a ninja in db
-router.put('/ninjas/:id',(req,res,next)=>{
-    res.send({type:'PUT'});
+router.put('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+        Ninja.findOne({ _id: req.params.id }).then((ninja) => {
+            res.send(ninja);
+        })
+    })
+
 })
 
 
 //delete a ninja in db
-router.delete('/ninjas/:id',(req,res,next)=>{
-    Ninja.findByIdAndRemove({_id:req.params.id}).then((ninja)=>{
+router.delete('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndRemove({ _id: req.params.id }).then((ninja) => {
         res.send(ninja);
     })
-    
+
 })
 
 
