38da27223b8d56593328b7771d168b53ead1d86c
Tushar <gupta.tus@husky.neu.edu>
2018-09-26 12:13:32 -0400

    MOndo Schema

---
+++ rest-api-mean/models/ninja.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const NinjaSchema = new Schema({
+    name:{
+        type:String,
+        required: [true,'Name is require']
+    },
+    rank:{
+        type:String
+    },
+    available:{
+        type:Boolean,
+        default:false
+    }
+});
+
+const Ninja = mongoose.model('ninja',)
